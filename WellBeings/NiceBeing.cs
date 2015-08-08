d7b583e68e59ab0eb7f185be457bbb4700f0481c
Gregory Kan <kan.gregory@gmail.com>
2015-08-08 15:47:44 +1200

    creation of being one - nicebeing

---
+++ WellBeings/NiceBeing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellBeings
+{
+   public  class NiceBeing
+    {
+       public string Name { get; set; }
+       public string OwnerName { get; set; }
+
+       public NiceBeing()
+       {
+           Console.WriteLine("Hey there, I'm a WellBeing, I'm here to help you. Please give me a name so that we can become more acquainted with one another");
+           this.Name = Console.ReadLine();
+           Console.WriteLine("Cool man! My name is" + this.Name);
+       }
+    }
+
+}
---
+++ WellBeings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WellBeings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NiceBeing newBeing = new NiceBeing();
+            Console.WriteLine(newBeing.Name);
+            Console.ReadLine();
+        }
+    }
+}
